f64418390f969b7689cd1d50b8a6651c50a7f0f3
Unknown <erick_andres27@hotmail.com>
2018-09-02 18:50:18 -0700

    Pagos generales

---
+++ IICAPS v1/DataObject/Pago.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IICAPS_v1.DataObject
+{
+    public class Pago
+    {
+        public int id { get; set; }
+        public string emisor { get; set; }
+        public DateTime fechaPago { get; set; }
+        public double cantidad { get; set; }
+        public string concepto { get; set; }
+        public string observaciones { get; set; }
+        public string recibio { get; set; }
+        public string estado { get; set; }
+
+        public string formatoFolio()
+        {
+            return id.ToString("0000000");
+        }
+    }
+}
