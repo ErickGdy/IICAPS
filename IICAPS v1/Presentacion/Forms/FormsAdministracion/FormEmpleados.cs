3910443a551365ae76df1d728eb050e52c3a814d
Erick Godoy <erick_andres27@hotmail.com>
2018-12-30 21:26:03 -0700

    Empleados CRUD

---
+++ IICAPS v1/Presentacion/Forms/FormsAdministracion/FormEmpleados.cs
@@ -0,0 +1,231 @@
+﻿using IICAPS_v1.Control;
+using IICAPS_v1.DataObject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IICAPS_v1.Presentacion
+{
+    public partial class FormEmpleados : Form
+    {
+        ControlIicaps control;
+        Usuario usuario;
+        Empleado empleado;  
+        public FormEmpleados(Empleado empleado, Usuario user)
+        {
+            InitializeComponent();
+            control = ControlIicaps.getInstance();
+            if (empleado == null)
+                this.empleado = new Empleado();
+            else
+            {
+                this.empleado = empleado;
+                txtMatricula.Text = empleado.Matricula;
+                txtNombre.Text = empleado.Nombre;
+                //txtFecha.Value = empleado.fecha;
+                txtCorreo.Text = empleado.Correo;
+                txtTelefono.Text = empleado.Telefono;
+                txtPuesto.Text = empleado.Puesto;
+            }
+            if (user != null)
+            {
+                this.usuario = user;
+                checkBox1.Checked = true;
+                cmbNivelAcceso.SelectedIndex = user.Nivel_Acceso;
+                txtUsuario.Text = user.Nombre_De_Usuario;
+                txtContraseña.Text = user.Contrasena;
+                txtContraseña2.Text = user.Contrasena;
+            }
+            else
+                this.usuario = new Usuario();
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (validarCampos())
+            {
+                if (validarContraseña())
+                {
+                    if(txtMatricula.Text=="")
+                        GenerarMatricula();
+                    empleado.Matricula = txtMatricula.Text;
+                    empleado.Nombre = txtNombre.Text;
+                    empleado.Telefono = txtTelefono.Text;
+                    empleado.Correo = txtCorreo.Text;
+                    empleado.Puesto = txtPuesto.Text;
+                    Usuario user_aux = usuario;
+                    if (checkBox1.Checked)
+                    {
+                        usuario.Matricula = txtMatricula.Text;
+                        usuario.Nombre_De_Usuario = txtUsuario.Text;
+                        usuario.Contrasena = txtContraseña.Text;
+                        usuario.Nivel_Acceso = cmbNivelAcceso.SelectedIndex;
+                    }
+                    else
+                    {
+                        if (usuario.Matricula != null)
+                        {
+                            try
+                            {
+                                if (control.eliminarUsuario(usuario.Matricula))
+                                    MessageBox.Show("Usuario desactivado!");
+                                else
+                                    MessageBox.Show("Error al desactivar usuario!");
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                        }
+                        user_aux = null;
+                    }
+                    try
+                    {
+                        if (this.empleado.ID != 0)
+                        {
+                            if (control.actualizarEmpleado(this.empleado, user_aux))
+                            {
+                                MessageBox.Show("Datos de empleado actualizados exitosamente!");
+                                Close();
+                                Dispose();
+                            }
+                            else
+                                MessageBox.Show("Error al guardar datos, verifique los campos y vuelva a intentarlo");
+                        }
+                        else
+                        {
+                            if (control.agregarEmpleado(this.empleado, user_aux))
+                            {
+                                MessageBox.Show("Datos guardados exitosamente!");
+                                Close();
+                                Dispose();
+                            }
+                            else
+                                MessageBox.Show("Error al guardar datos, verifique los campos y vuelva a intentarlo");
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }else
+                    MessageBox.Show("La contraseña no coincide, verifique los campos y vuelva a intentarlo");
+            }
+            else
+                MessageBox.Show("No dejar campos vacios");
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private bool validarCampos()
+        {
+            if (txtNombre.Text != "" && txtTelefono.Text != "" && txtCorreo.Text != "" )
+                return true;
+            return false;
+        }
+        private bool validarContraseña()
+        {
+            if (checkBox1.Checked)
+            {
+                if (txtContraseña.Text == txtContraseña2.Text)
+                    return true;
+                else
+                {
+                    txtContraseña.Text = "";
+                    txtContraseña2.Text = "";
+                    return false;
+                }
+            }
+            else
+                return true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtUsuario.Enabled = true;
+                txtContraseña.Enabled = true;
+                txtContraseña2.Enabled = true;
+                cmbNivelAcceso.Enabled = true;
+            }else
+            {
+                txtUsuario.Enabled = false;
+                txtContraseña.Enabled = false;
+                txtContraseña2.Enabled = false;
+                cmbNivelAcceso.Enabled = false;
+            }
+        }
+
+        private string GenerarMatricula()
+        {
+            string matricula = "";
+            string validacion = "";
+            do {
+                string[] nombre = txtNombre.Text.Split(' ');
+                string iniciales = "";
+                int numeroLetra = 0;
+                do
+                {
+                    for (int i = 0; i < nombre.Count(); i++)
+                    {
+                        try
+                        {
+                            iniciales += nombre[i].Substring(numeroLetra, 1);
+                        }
+                        catch (Exception re)
+                        {
+                            iniciales += nombre[i].Substring(nombre[i].Length-1, 1);
+                        }
+                    }
+                    numeroLetra++;
+                } while (iniciales.Length < 4);
+                Random rdm = new Random();
+                matricula =rdm.Next(1, 99).ToString("00") + "-" + iniciales.Substring(0,4).ToUpper() + "-" + rdm.Next(1, 9999).ToString("0000");
+                validacion = control.validarMatricula(matricula);
+            } while (validacion!=null);
+            txtMatricula.Text= matricula;
+            return matricula;
+        }
+
+        private void onlyNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            if (Char.IsDigit(e.KeyChar) || Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            };
+        }
+
+
+        private void noSpaces_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+            }
+        }
+    }
+}
---
+++ IICAPS v1/Presentacion/Mains/Administracion/MainEmpleados.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IICAPS_v1.Control;
+using IICAPS_v1.DataObject;
+using IICAPS_v1.Presentacion;
+using MySql.Data.MySqlClient;
+
+namespace IICAPS_v1.Presentacion.Mains.Psicoterapia
+{
+    public partial class MainEmpleados : Form
+    {
+
+        private static MainEmpleados instance;
+        ControlIicaps control;
+        public MainEmpleados()
+        {
+            InitializeComponent();
+            control = ControlIicaps.getInstance();
+            try
+            {
+                actualizarTabla(control.obtenerEmpleadosTable());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public static MainEmpleados getInstance()
+        {
+            if (instance == null)
+                instance = new MainEmpleados();
+            return instance;
+        }
+
+        private void actualizarTabla(MySqlDataAdapter data)
+        {
+            try
+            {
+                DataTable dtDatos = new DataTable();
+                //Con la informacion del adaptador se llena el datatable
+                data.Fill(dtDatos);
+                //Se asigna el datatable como origen de datos del datagridview
+                dataGridView1.DataSource = dtDatos;
+                //Actualiza el valor del ancho de la columnas
+                int x = (dataGridView1.Width - 20) / (dataGridView1.Columns.Count-1);
+                foreach (DataGridViewColumn aux in dataGridView1.Columns)
+                {
+                    aux.Width = x;
+                }
+                dataGridView1.Columns[0].Visible = false; ;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            FormEmpleados fa = new FormEmpleados(null,null);
+            fa.FormClosed += new FormClosedEventHandler(form_Closed);
+            fa.Show();
+        }
+
+        private void form_Closed(object sender, FormClosedEventArgs e)
+        {
+            actualizarTabla(control.obtenerEmpleadosTable(txtBuscar.Text));
+        }
+
+        private void consultarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String id = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                //Paciente paciente = control.consultarPaciente(id);
+                //ReporteDePaciente fa = new ReporteDePaciente(id);
+                //fa.FormClosed += new FormClosedEventHandler(form_Closed);
+                //fa.Show();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String id = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                Empleado Empleado = control.consultarEmpleado(id);
+                Usuario usuario = control.consultarUsuario(id);
+                FormEmpleados fa = new FormEmpleados(Empleado,usuario);
+                fa.FormClosed += new FormClosedEventHandler(form_Closed);
+                fa.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void cancelarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String matricula = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                DialogResult dialogresult = MessageBox.Show("¿Desea eliminar Empleado?", "Eliminar Empleado", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if(dialogresult == DialogResult.OK)
+                {
+                    if (control.desactivarEmpleado(matricula))
+                    {
+                        MessageBox.Show("Empleado eliminado");
+                        actualizarTabla(control.obtenerEmpleadosTable());
+                    }
+                    else
+                        MessageBox.Show("Error al eliminar Empleado");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void limpiarBusqueda_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            limpiarBusqueda.Visible = false;
+            txtBuscar.Text = "";
+            try
+            {
+                actualizarTabla(control.obtenerEmpleadosTable());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            actualizarTabla(control.obtenerEmpleadosTable(txtBuscar.Text));
+        }
+
+        private void txtBuscar_KeyUp(object sender, KeyEventArgs e)
+        {
+            string texto = txtBuscar.Text;
+            if (texto != "")
+            {
+                limpiarBusqueda.Visible = true;
+                if (texto.Length > 2)
+                {
+                    try
+                    {
+                        actualizarTabla(control.obtenerEmpleadosTable(texto));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                limpiarBusqueda.Visible = false;
+                try
+                {
+                    actualizarTabla(control.obtenerEmpleadosTable());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+           
+        }
+
+        private void Main_SizeChanged(object sender, EventArgs e)
+        {
+            int ancho = this.Width;
+            //Actualiza el tamaño de la tabla con respecto al tamaño de la ventana
+            dataGridView1.Width = ancho - 195;
+            dataGridView1.Height = this.Height - 130;
+            //actualiza la posicion de los controles con respecto al tamaño de la ventana
+            btnAgregar.Location = new Point(ancho - 169, btnAgregar.Location.Y);
+            txtBuscar.Location = new Point(ancho - 216, txtBuscar.Location.Y);
+            pictureBoxBuscar.Location = new Point(ancho - 245, pictureBoxBuscar.Location.Y);
+            limpiarBusqueda.Location = new Point(ancho - 39, limpiarBusqueda.Location.Y);
+            //Actualiza el valor del ancho de la columnas
+            if (dataGridView1.Columns.Count != 0)
+            {
+                int x = (dataGridView1.Width - 20) / (dataGridView1.Columns.Count-1);
+                foreach (DataGridViewColumn aux in dataGridView1.Columns)
+                {
+                    aux.Width = x;
+                }
+            }
+        }
+
+    }
+}
